a7eb9f6fbc4656120dcdd7c1b5311f2f3f66e4c8
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-02-09 20:29:49 -0300

    ejercicio 6, 7, 8, 9 y 10.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var importe;
+	var aumento;
+	var resultado;
 
-	
+	importe=document.getElementById('sueldo').value;
+
+	importe=parseInt(importe);
+
+	aumento=importe*0.1;
+
+	resultado=importe+aumento;
+
+	document.getElementById('resultado').value=resultado;
 }
