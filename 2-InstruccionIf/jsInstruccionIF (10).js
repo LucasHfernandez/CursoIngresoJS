dbee5f70b4f52ba40406d56e2f8e2d163f226fd2
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-02-15 21:52:04 -0300

    Ejercicios 6, 7, 8, 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
 
+
+	numero=Math.floor(Math.random() * 11);
+
+
+	if(numero >8)
+	{
+		alert("EXCELENTE: " + numero);
+	}
+	else
+	{
+		if(numero >3)
+		{
+			alert("APROBÓ: " + numero);
+		}
+		else
+		{
+			alert("Vamos, la proxima se puede: " + numero);
+		}
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
 
 
-
-
+	if(edad >17)
+	{
+		alert("Es Mayor de edad");
+	}
+	else
+	{
+		if(edad >12 && edad <18)
+		{
+			alert("es adolescente");
+		}
+		else
+		{
+			alert("Es menor de edad");
+		}
+	}
 }//FIN DE LA FUNCIÓN
