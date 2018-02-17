26d22841b67184c986847b23d8b93c75627e229d
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-02-16 22:14:32 -0300

    Todos los ejercicios(completos)

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,29 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-
-
-
+	switch(mesDelAño)
+	{
+		case "Enero" :
+		{
+			alert("Que comiences bien el año");
+			break;
+		}
+		case "Marzo" :
+		{
+			alert("A clases!!!");
+			break;
+		}
+		case "Julio" :
+		{
+			alert("Se viene las vacaciones!!!");
+			break;
+		}
+		case "Diciembre" :
+		{
+			alert("Felices fiestas!!!");
+			break;
+		}
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,30 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
-
-
-
+	switch(mesDelAño)
+	{
+		case "Febrero" :
+		{
+			alert("Si tiene 28 dias");
+			break;
+		}
+		case "Abril" :
+		{}
+		case "Junio" :
+		{}
+		case "Septiembre" :
+		{}
+		case "Noviembre" :
+		{
+			alert("Si tiene 30 dias");
+			break;
+		}
+		default:
+		{
+			alert("Si tiene 31 dias");
+		}
+	}
 }//FIN DE LA FUNCIÓN
