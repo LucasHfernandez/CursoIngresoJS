1a10c8398af4f7786e263829ac8d4c28749200f3
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-02-16 22:16:59 -0300

    ejercicio 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,31 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	
+	numeroUno=prompt("Ingrese primero numero");
+	numeroDos=prompt("Ingrese segundo numero");
 
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	if(numeroUno == numeroDos)
+	{
+		resultado= numeroUno * numeroDos;
+		document.write(resultado);
+	}
+	else
+	{
+		if(numeroUno > numeroDos)
+		{
+			resultado= numeroUno - numeroDos;
+			document.write(resultado);
+		}
+		else
+		{
+			resultado= numeroUno + numeroDos;
+			document.write(resultado);
+		}
+	}
 }
