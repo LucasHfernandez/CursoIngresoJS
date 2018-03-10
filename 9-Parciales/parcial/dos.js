78e1191cf3dcd7e5fce0122386568eaa87390350
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-03-10 10:31:58 -0300

    Repaso

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
-  
+  	var largo;
+	var ancho;
+	var perimetro;
+	var alambrado;
+
+	largo = document.getElementById('alrgo').value;
+	ancho = document.getElementById('ancho').value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = (largo + ancho) * 2;
+
+	alambrado = perimetro * 3;
+
+	alert("Se necesitan: " + alambrado + " metros de alambre.");
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+	var precio;
+	var descuento;
+	var iva;
+	var precioFinal;
 
+	precio = document.getElementById('alrgo').value;
+	descuento = document.getElementById('ancho').value;
+
+	precio = parseInt(precio);
+	decuento = parseInt(descuento);
+
+	descuento = (precio * descuento) / 100;
+
+	precio = precio - descuento;
+
+	iva = (precio * 21) / 100;
+
+	precioFinal = precio + iva;
+
+	alert("El precio final es: " + precioFinal);
 }
+
+
+/* si quiero tomar valores, tengo que usar el document.getelementbyid del lado derecho, 
+pero si quiero que esos valores se coloquen en la pantalla (sin alert o prompt o document.log)
+el document.getelementbyid se coloca del lado izquierdo. */
