ef47cedd23e3b78340e063155f260823d9a62e28
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2018-07-05 22:01:42 -0300

    Ejercicios

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,22 @@
-function Mostrar()
+function mostrar()
 {
+	var precio;
+	var descuento;
+	var iva;
+	var precioFinal;
 
+	precio = prompt("Ingrese el precio.");
+	descuento = prompt ("Ingrese el descuento.");
+
+	iva = (precio * 21) / 100;
+
+	descuento = precio * descuento / 100;
+
+	precioFinal = (precio - descuento) + iva;
+
+	precio = precio - descuento;
+
+	alert("El descuento es: " + descuento + " el precio total es: " + precio + " y el iva es: " + iva);
+
+	document.getElementById('elPrecioFinal').value = precioFinal;
 }
--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,20 @@
 
-function Mostrar()
+function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
 
+	base = prompt("Ingrese la base del triangulo equilatero.");
+	altura = prompt ("Ingrese la altura del triangulo equilatero");
+
+	base = parseInt(base);
+	altura = parseInt(altura);
+
+	perimetro = base * 3;
+
+	superficie = base * altura;
+
+	alert("La superficie es: " + superficie + " y el perimetro es: " + perimetro); 
 }
